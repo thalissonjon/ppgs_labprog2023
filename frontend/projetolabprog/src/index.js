1e2df97a71282a77365bf0e9694c0dd47ea39991
Thalisson <55109282+thalissonjon@users.noreply.github.com>
2023-07-11 03:55:45 -0300

    ATT

--- frontend/projetolabprog/src/index.js
+++ frontend/projetolabprog/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Home from './views/Home';
+
+/* não vi como fazer somente admin-lte*/
+import './admin-lte/dist/css/adminlte.min.css'
+import './admin-lte/plugins/fontawesome-free/css/all.min.css'
+import './admin-lte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css'
+import './admin-lte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css'
+import './admin-lte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
