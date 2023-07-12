490c7ef7432c37694fa9651e9eafade579883bd6
Thalisson <55109282+thalissonjon@users.noreply.github.com>
2023-07-12 15:01:22 -0300

    att

---
+++ frontend/projetolabprog/src/componentes/login/Login.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Login.css';
+
+async function loginUser(credentials) {
+ return fetch('http://localhost:8080/login', {
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json'
+   },
+   body: JSON.stringify(credentials)
+ })
+   .then(data => data.json())
+}
+
+export default function Login({ setToken }) {
+  const [usuario, setUsuario] = useState();
+  const [senha, setSenha] = useState();
+
+  const handleSubmit = async e => {
+    
+    e.preventDefault();
+    const token = await loginUser({
+      usuario,
+      senha
+    });
+    
+
+    
+    setToken(token);
+  }
+
+  return(
+    <div className="login-wrapper">
+      <div className="sppg-container" >
+      <h2 style={{textAlign: "center"}}>SPPG </h2>
+      </div>
+      <form onSubmit={handleSubmit}>
+      <div className="input-container">
+          <label >Usuario</label>
+          <input type="text" onChange={e => setUsuario(e.target.value)} />
+        </div>
+        <div className="input-container">
+      
+          <label>Senha</label>
+          <input type="password" onChange={e => setSenha(e.target.value)} />
+
+        </div>
+        <div className="button-container">
+          <button type="submit">Entrar</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
+};
+
+
+
+
+
--- frontend/projetolabprog/src/rotas/Rotas.js
+++ frontend/projetolabprog/src/rotas/Rotas.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
 import {Route, Routes, HashRouter} from 'react-router-dom'
 
 import Docente from '../views/Docente'
 import Inicio from '../views/inicio'
 import Programa from '../views/programa'
 import Producao from '../views/Producao'
+import Login from '../componentes/login/Login';
+import Dashboard from '../componentes/login/Dashboard';
 
 export default function Rotas() {
+    const [token, setToken] = useState();
+
+    if(!token) {
+        return <Login setToken={setToken} />
+      }
+
     return (
         <HashRouter>
             <Routes>
-                <Route path="/" element={<Inicio/>} />
+                <Route path="/inicio" element={<Inicio/>} />
                 <Route path="/programa" element={<Programa/>} />
                 <Route path="/docente" element={<Docente/>} /> 
                 <Route path="/producao" element={<Producao/>} />        
